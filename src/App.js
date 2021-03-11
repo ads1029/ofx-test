f1829b5da17f69d098bcc1187313e0b2bc52ef7c
Minjie Huang <millerk1029@gmail.com>
2021-03-11 16:00:11 +1100

    Init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Quick Quote</h1>
       </header>
+
+      <body>
+        <p>first name</p>
+        <p>last name</p>
+        <p>email</p>
+        <p>phone num</p>
+        <p>from currency</p>
+        <p>to currency</p>
+        <p>amount</p>
+        <button>GET QUOTE</button>
+      </body>
     </div>
   );
 }
