fbeeaa195fb9903ec1c45f89f2fcffcecb0a1009
Minjie Huang <millerk1029@gmail.com>
2021-03-12 17:51:55 +1100

    Revert "Prepare to add useContext hook"
    This reverts commit 10e59df4d87e919387494134ecdc8a6601719a40.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import "./App.css";
-
-import Result from "./Result";
-import Quote from "./Quote";
 function App() {
-  const [fromAmount, setFromAmount] = useState("10000");
-
-  // let fromAmount = "100000";
-  let fromCurrency = "AUD";
-  let toCurrency = "USD";
-
   return (
-    <Router>
-      <div className="App">
-        <h1 id="quote">Quick Quote</h1>
-        <Switch>
-          <Route path="/" exact>
-            <Quote
-              fromAmount={fromAmount}
-              fromCurrency={fromCurrency}
-              toCurrency={toCurrency}
-            />
-          </Route>
-          <Route path="/result">
-            <Result
-              fromAmount={fromAmount}
-              fromCurrency={fromCurrency}
-              toCurrency={toCurrency}
-            />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <h1>Quick Quote</h1>
+      </header>
+
+      <body>
+        <p>first name</p>
+        <p>last name</p>
+        <p>email</p>
+        <p>phone num</p>
+        <p>from currency</p>
+        <p>to currency</p>
+        <p>amount</p>
+        <button>GET QUOTE</button>
+      </body>
+    </div>
   );
 }
 
